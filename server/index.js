c2915ad344f988b43bde38b91db9b03614f93d4e
Matthew <m_kmorgan@hotmail.com>
2021-03-16 22:22:27 -0600

    created a basic boiler plate to add graphQL into the server

--- server/index.js
+++ server/index.js
@@ -2,12 +2,32 @@ import express from 'express';
 const app = express();
 const PORT = 8080;
 import path from 'path';
+import { buildSchema } from 'graphql';
+import { graphqlHTTP } from 'express-graphql';
 
+/* SETTING UP  */
+const schema = buildSchema(`
+  type Query {
+    getName(name: String): String
+  }
+`);
+
+var root = {
+  getName: ({name}) => {
+    return name
+  }
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../public')));
 
 
-
 app.listen(PORT , () => {
-  console.log(`listening on port: ${PORT}`);
+  console.log(`listening on port: ${PORT}
+  Running a GraphQL API server at http://localhost:${PORT}/graphql`);
 })
