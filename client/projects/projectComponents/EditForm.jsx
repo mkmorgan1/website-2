7006c7ef6ad6d670a59564f21a969f2543badbd6
Matthew <m_kmorgan@hotmail.com>
2021-04-14 17:43:56 -0600

    added a boilerPlate for the edit form

--- client/projects/projectComponents/EditForm.jsx
+++ client/projects/projectComponents/EditForm.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
 const EditForm = ({app, styles, edit}) => {
+  const nameEl = useRef(null);
+  const [name, setName] = useState(app.name)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = {
+      name: nameEl.current.value
+    }
+    console.log(formData)
+  }
   return (
     <>
       <div onClick={edit}>ESC</div>
-      <form action="">
-        <input type="text"/>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name='name' id='name' value={name} ref={nameEl} onChange={() => setName(nameEl.current.value)}/>
+        <button type='submit'>edit</button>
       </form>
     </>
 
