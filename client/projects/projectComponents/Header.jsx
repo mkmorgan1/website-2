fd65343a4a067bd7db432b1aa239b02d61a520ac
Matthew <m_kmorgan@hotmail.com>
2021-04-19 19:35:23 -0600

    fixed SASS to better size for mobile

--- client/projects/projectComponents/Header.jsx
+++ client/projects/projectComponents/Header.jsx
@@ -4,8 +4,18 @@ const Header = ({styles, github, deployed, name}) => {
   return (
     <div>
         <h2 className={styles.projectHeaderName}>{name} |</h2>
-        <a href={github} target='_blank'><i class='fab fa-github'></i></a>
-        {deployed && <a href={deployed} target='_blank'><i class='fas fa-external-link-alt'></i></a>}
+        <span>{'  '}
+          <a href={github} target='_blank'>
+            <i class='fab fa-github'></i>
+          </a>{'  '}
+        </span>
+        {deployed &&
+          <span>{'  '}
+            <a href={deployed} target='_blank'>
+              <i class='fas fa-external-link-alt'></i>
+            </a>
+          </span>
+        }
     </div>
   );
 }
